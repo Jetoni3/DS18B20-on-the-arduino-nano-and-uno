836f01c247da9757174009fba6d0a99002ab3900
Jetoni3 <36087281+Jetoni3@users.noreply.github.com>
2023-03-17 14:26:24 +0100

    Update arduino_nano_sensor_DS18B20.ino

--- arduino_nano_sensor_DS18B20.ino
+++ arduino_nano_sensor_DS18B20.ino
@@ -1,30 +1,37 @@
  
+// This is a code made especially for the DS18B20 Temprature sensor made by Jeton
+// It might work with other sensors since it uses a library for multiple sensors
+// This works with the arduino nano and uno and might work with a board with a esp32 chip
 #include <OneWire.h>
 #include <DallasTemperature.h>
- float temp = 0.0;
- int oneWireBus = 12;
- OneWire oneWire(oneWireBus);
- DallasTemperature sensors(&oneWire);
- OneWire oneWire(oneWireBus);
- DallasTemperature sensors(&oneWire);
- void setup() {
-     Serial.begin(9600);
-     Serial.println("1-wire temperatuur sensor");
-     sensors.begin();
- }
- void loop() {
- 
-     sensors.requestTemperatures();
-     temp = sensors.getTempCByIndex(0);
- 
-     Serial.print("Temperatuur is: ");
-     Serial.println(temp);
- 
-     delay(1000);
- 
- }
- sensors.requestTemperatures();
- temp = sensors.getTempCByIndex(0);
- Serial.print("Temperatuur is: ");
- Serial.println(temp);
- delay(1000);
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // Start the Serial Monitor
+  Serial.begin(115200);
+  // Start the DS18B20 sensor
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  // Define temperatureC and temperatureF 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  // Output of the temperature on your Serial Monitor
+  Serial.print("De temperatuur is: ");
+  Serial.print(temperatureC); 
+  Serial.print("ºC, "); 
+  Serial.print(temperatureF); 
+  Serial.println("ºF"); 
+  // The amount of ms delay after every temperature reading before looping
+  delay(10000);
+}
